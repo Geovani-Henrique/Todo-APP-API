d90299e0229e835cb12943d9864d35ed0fdb249d
Geovani-Henrique <113391254+Geovani-Henrique@users.noreply.github.com>
2023-02-15 19:35:23 -0300

    Add files via upload

--- src/DAO/UsuarioDAO.js
+++ src/DAO/UsuarioDAO.js
@@ -1,10 +1,13 @@
 // Importa o bd.js para poder usar o banco de dados simulado
 const db = require("../infra/db");
 
+
 // Essa classe encapsula o acesso ao Banco de Dados. Todos os métodos abaixos são estáticos. Isso quer dizer que não precisamos instanciar a classe para usá-los e serão chamados pela classe UsuarioController... Alguns vão dar retorno e para outros, isso não será necessário
 class UsuarioDAO {
+
+  // GET  --  Função ALL - Retorna todas as linhas. No callback existe o argumento ROWS
   static listar() {
-    const query = "SELECT * FROM USUARIOS";
+    const query = "SELECT * FROM USUARIOS"; 
     return new Promise((resolve, reject) => {
       db.all(query, (err, rows) => {
         if (err) {
@@ -15,6 +18,23 @@ class UsuarioDAO {
     });
   }
 
+
+  // GET  --  Função GET - Retorna APENAS UMA linha que será a linha do usuário dono do email que passamos por parâmetro. No callback existe o argumento ROW, no SINGULAR
+  static buscarPorEmail(email) {
+    const query = "SELECT * FROM USUARIOS WHERE email = ?";
+
+    return new Promise((resolve, reject) => {
+      db.get(query, [email], (err, row) => {
+        if (err) {
+          reject(false);
+        }
+        resolve(row);
+      });
+    });
+  }
+
+
+  // POST  --  Função RUN - Executa a função. No callback NÂO existe o argumento ROWS, apenas o argumento ERR, porém devolvemos os usuário
   static inserir(usuario) {
     const query = `INSERT INTO USUARIOS (nome, email, senha) VALUES (?, ?, ?)`;
 
@@ -31,18 +51,8 @@ class UsuarioDAO {
     });
   }
 
-  static buscarPorEmail(email) {
-    const query = "SELECT * FROM USUARIOS WHERE email = ?";
-    return new Promise((resolve, reject) => {
-      db.get(query, [email], (err, row) => {
-        if (err) {
-          reject(false);
-        }
-        resolve(row);
-      });
-    });
-  }
 
+  // PUT  --   Função RUN - Executa a função. No callback NÂO existe o argumento ROWS, apenas o argumento ERR. Se tudo der certo, devolve o objeto: { mensagem: "Usuário atualizado com sucesso" }
   static atualizar(email, usuario) {
     const query =
       "UPDATE USUARIOS SET nome = ?, email = ?, senha = ? WHERE email = ?";
@@ -63,7 +73,9 @@ class UsuarioDAO {
     });
   }
 
-  static deletar(email) {
+
+  // DELETE  --  Função RUN - Executa a função. No callback NÂO existe o argumento ROWS e nem ROW. Existe apenas o argumento ERR. Se tudo der certo, devolve o objeto: { mensagem: "Usuário deletado com sucesso", email: email }
+  static deletar(email) {    
     const query = "DELETE FROM USUARIOS WHERE email = ?";
     return new Promise((resolve, reject) => {
       db.run(query, [email], (err) => {
@@ -73,12 +85,12 @@ class UsuarioDAO {
             erro: err,
           });
         }
-
         resolve({ mensagem: "Usuário deletado com sucesso", email: email });
       });
     });
   }
 }
+
 
 // Exporta a classe
 module.exports = UsuarioDAO;
